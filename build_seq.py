a351e1442f4e066ce215b044e1252ad217b68a7d
Riolu <1226097512@qq.com>
2017-07-27 18:53:20 +0800

    divide the time into ten pieces for some reason

--- build_seq.py
+++ build_seq.py
@@ -5,6 +5,7 @@ path = "data\\imote-traces-cambridge\\SR-10mins-Students"
 
 contact_list = [] # the contents are in the form [meet_time, user1, user2], where user1 meets user2.
 
+
 # ID [1:36] are users; [37:54] are locations
 for i in range(1, 37):
     filename = str(i)+".dat"
@@ -30,46 +31,80 @@ max_time = contact_list[-1][0]
 
 print (len(contact_list))
 
-
-user_seqs = {}
-for user in range(1,37):
-    user_seqs[user] = {} # seqs for a certain users are stored in dictionary
-
-
-count = 500
+# This part is for display the meeting time
+time_dict = {}
+meet_place_dict = {}
+gap = (max_time-min_time)/10
 for meet in contact_list:
-    user1 = meet[1]
-    user2 = meet[2]
-
-    # user2 is a location
-    if user2>36 and user2<=54:
-        seqs_dict = user_seqs[user1]
-        if str(user1) not in seqs_dict:
-            seqs_dict[str(user1)] = []
-        seqs_list = seqs_dict[str(user1)]
-        if user2 not in seqs_list:
-            seqs_list.append(user2)
-
-
+    tmp = int((meet[0]-min_time)/gap)
+    if tmp not in time_dict:
+        time_dict[tmp] = 1
     else:
-    # here user1 detects user2, then the sequences of users2 are copied and add to user1
-        seqs_dict = user_seqs[user2]
-        for users in seqs_dict:
-            # variable users is string while objects is list
-            objects = seqs_dict[users]
+        time_dict[tmp] = time_dict[tmp] + 1
 
-            if users == (str(user1)+','+str(user2)): #?
-                continue
-
-            new_users = str(users)+','+str(user1)
-            user_seqs[user1][new_users] = objects
-
-    count = count-1
-    if count==0:
-        break
+    user2 = meet[2]
+    if user2 > 36 and user2 <= 54:
+        if tmp not in meet_place_dict:
+            meet_place_dict[tmp] = 1
+        else:
+            meet_place_dict[tmp] = meet_place_dict[tmp] + 1
 
 
-print (user_seqs)
+for i in range(0,10):
+    print (time_dict[i], meet_place_dict[i])
 
 
+
+
+
+# max_user_num = 5
+# user_seqs = {}
+# for user in range(1,37):
+#     user_seqs[user] = {} # seqs for a certain users are stored in dictionary
+#
+#
+# run_time = 5000
+# count = 0
+# for meet in contact_list:
+#     user1 = meet[1]
+#     user2 = meet[2]
+#
+#     # user2 is a location
+#     if user2>36 and user2<=54:
+#         seqs_dict = user_seqs[user1]
+#         if str(user1) not in seqs_dict:
+#             seqs_dict[str(user1)] = []
+#         seqs_list = seqs_dict[str(user1)]
+#         if user2 not in seqs_list:
+#             seqs_list.append(user2)
+#
+#
+#     else:
+#     # here user1 detects user2, then the sequences of users2 are copied and add to user1
+#         seqs_dict = user_seqs[user2]
+#         for users in seqs_dict:
+#             # variable users is string while objects is list
+#             objects = seqs_dict[users]
+#
+#             previous = users.split(',')
+#             if len(previous)==max_user_num: # if the users number exceeds, continue
+#                 continue
+#             if str(user1) in previous: # user1 appears in one suquence of user2 = =
+#                 continue
+#
+#             new_users = str(users)+','+str(user1)
+#             user_seqs[user1][new_users] = objects
+#
+#     count = count+1
+#     if (count%int(run_time/100)==0):
+#         print (count/int(run_time/100))
+#     if count==run_time:
+#         break
+#
+#
+#
+# # for i in user_seqs:
+# #     print (user_seqs[i])
+#
+#
 
